d33e230d1b52619a661ca881ec4ed9e37023a2b8
ksshivoham <shivamkaushik59@gmail.com>
2019-04-18 23:58:05 +0530

    intial commit 2

--- prodcon.c
+++ prodcon.c
@@ -1,36 +1,67 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-struct shmem_structure
- { 
-     int field1; 
-     int field2;
- };
+#include<stdio.h>
+#include<stdlib.h>
 
- int main()
- {
-struct shmem_structure *shptr = get_shared_memory_structure();
-while(1) 
+int mutex=1,full=0,empty=3,x=0;
+
+int main()
 {
+	int n;
+	void producer();
+	void consumer();
+	int wait(int);
+	int signal(int);
+	printf("\n1.Producer\n2.Consumer\n3.Exit");
+	while(1)
+	{
+		printf("\nEnter your choice:");
+		scanf("%d",&n);
+		switch(n)
+		{
+			case 1:	if((mutex==1)&&(empty!=0))
+						producer();
+					else
+						printf("Buffer is full!!");
+					break;
+			case 2:	if((mutex==1)&&(full!=0))
+						consumer();
+					else
+						printf("Buffer is empty!!");
+					break;
+			case 3:
+					exit(0);
+					break;
+		}
+	}
 
-     int produced = produceNext();
-     int produced = produceNext(); 
-     shptr->field1=produced;
-     shptr->field2 = 1;
-     printf("p");
+	return 0;
 }
-struct shmem_structure *shptr = get_shared_memory_structure();
-while(1) 
-{ 
-     int consumed;
-     consumeNext(consumed);
-     while(shptr->field2 == 0);
-     consumed=shptr->field1;
-     consumeNext(consumed);
-     shptr->field2 = 0;
-     printf("c");
-    
-}
-return 0;
 
- }
+int wait(int s)
+{
+	return (--s);
+}
+
+int signal(int s)
+{
+	return(++s);
+}
+
+void producer()
+{
+	mutex=wait(mutex);
+	full=signal(full);
+	empty=wait(empty);
+	x++;
+	printf("\nProducer produces the item %d",x);
+	mutex=signal(mutex);
+}
+
+void consumer()
+{
+	mutex=wait(mutex);
+	full=wait(full);
+	empty=signal(empty);
+	printf("\nConsumer consumes item %d",x);
+	x--;
+	mutex=signal(mutex);
+}
