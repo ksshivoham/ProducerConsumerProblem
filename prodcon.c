7c71514a566c1ebd18df9b2dacaf4260c5af44ba
ksshivoham <shivamkaushik59@gmail.com>
2019-04-22 14:42:55 +0530

    intial commit 6

--- prodcon.c
+++ prodcon.c
@@ -1,57 +1,61 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/shm.h>
-#include <unistd.h>
-#include <pthread.h>
+# include <stdio.h>
+# include <pthread.h>
+# define BufferSize 10
 
-void *produceNext();
-void *consumeNext();
-int x=1;
+void *Producer();
+void *Consumer();
 
-int produced,consumed;
-pthread_mutex_t l;
+int BufferIndex=0;
+char *BUFFER;
+
+pthread_cond_t Buffer_Not_Full=PTHREAD_COND_INITIALIZER;
+pthread_cond_t Buffer_Not_Empty=PTHREAD_COND_INITIALIZER;
+pthread_mutex_t mVar=PTHREAD_MUTEX_INITIALIZER;
 
 int main()
 {
+    pthread_t ptid,ctid;
 
-    pthread_mutex_init(&l,NULL);
-    pthread_t p,c;
-    pthread_create(&p,NULL,produceNext(),NULL);
-    pthread_create(&c,NULL,consumeNext(),NULL);
-    pthread_join(p,NULL);
-    pthread_join(c,NULL);
+    BUFFER=(char *) malloc(sizeof(char) * BufferSize);
 
+    pthread_create(&ptid,NULL,Producer,NULL);
+    pthread_create(&ctid,NULL,Consumer,NULL);
+
+    pthread_join(ptid,NULL);
+    pthread_join(ctid,NULL);
+
+
+    return 0;
+}
+
+void *Producer()
+{
+    for(;;)
+    {
+        pthread_mutex_lock(&mVar);
+        if(BufferIndex==BufferSize)
+        {
+            pthread_cond_wait(&Buffer_Not_Full,&mVar);
+        }
+        BUFFER[BufferIndex++]='@';
+        printf("Produce : %d \n",BufferIndex);
+        pthread_mutex_unlock(&mVar);
+        pthread_cond_signal(&Buffer_Not_Empty);
+    }
 
 }
-    void *produceNext()
-    {
-        for (int i = 0; i <5; i++) {
 
-
-        pthread_mutex_lock(&l);
-        x++;
-        produced=x;
-        printf("%d",produced);
-        pthread_mutex_unlock(&l);
-        }
-
-
-    }
-    void *consumeNext()
+void *Consumer()
 {
-    for (int i = 0; i < 5; i++) {
-
-        pthread_mutex_lock(&l);
-        consumed=x;
-        printf("%d",consumed);
-        x--;
-        pthread_mutex_unlock(&l);
+    for(;;)
+    {
+        pthread_mutex_lock(&mVar);
+        if(BufferIndex==-1)
+        {
+            pthread_cond_wait(&Buffer_Not_Empty,&mVar);
+        }
+        printf("Consume : %d \n",BufferIndex--);
+        pthread_mutex_unlock(&mVar);
+        pthread_cond_signal(&Buffer_Not_Full);
     }
-
-
-    }
-
-
-
-
+}
